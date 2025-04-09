42b2695089b69725ce9309138f8a1872a60068ad
vip71 <107436192+vip71@users.noreply.github.com>
2025-04-09 16:29:19 +0200

    add starting match

--- src/main/java/Match.java
+++ src/main/java/Match.java
@@ -1,2 +1,18 @@
 public class Match {
+  private final String homeTeam;
+
+  private final String awayTeam;
+
+  public Match(String homeTeam, String awayTeam) {
+    this.homeTeam = homeTeam;
+    this.awayTeam = awayTeam;
+  }
+
+  public String getHomeTeam() {
+    return homeTeam;
+  }
+
+  public String getAwayTeam() {
+    return awayTeam;
+  }
 }
--- src/main/java/Scoreboard.java
+++ src/main/java/Scoreboard.java
@@ -1,8 +1,21 @@
+import java.util.LinkedList;
+import java.util.Objects;
+
 public class Scoreboard {
+
+  LinkedList<Match> startedMatches = new LinkedList<>();
+
   public void startMatch(String homeTeam, String awayTeam) {
+    Match match = new Match(homeTeam, awayTeam);
+    startedMatches.add(match);
   }
 
   public Match getMatch(String homeTeam, String awayTeam) {
+    for (Match match : startedMatches) {
+      if(Objects.equals(match.getHomeTeam(), homeTeam) && Objects.equals(match.getAwayTeam(), awayTeam)){
+        return match;
+      }
+    }
     return null;
   }
 }
