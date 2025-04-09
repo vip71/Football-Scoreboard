7dd30adeeef9ed4627e4743661cd18c8b9ef150d
vip71 <107436192+vip71@users.noreply.github.com>
2025-04-09 16:18:37 +0200

    add starting match test

---
+++ src/main/java/Scoreboard.java
@@ -0,0 +1,8 @@
+public class Scoreboard {
+  public void startMatch(String homeTeam, String awayTeam) {
+  }
+
+  public Match getMatch(String homeTeam, String awayTeam) {
+    return null;
+  }
+}
---
+++ src/test/java/TestStartingMatch.java
@@ -0,0 +1,25 @@
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TestStartingMatch {
+
+  @DataProvider(name = "teams")
+  public Object[][] teams() {
+    return new Object[][]{
+        {"Poland", "Denmark"},
+    };
+  }
+
+  @Test(dataProvider = "teams")
+  public void testStartingMatch(String homeTeam, String awayTeam) {
+    //Given Scoreboard is created
+    Scoreboard scoreboard = new Scoreboard();
+    //When Match is started
+    scoreboard.startMatch(homeTeam, awayTeam);
+    //Then match is saved in scoreboard
+    Match match = scoreboard.getMatch(homeTeam, awayTeam);
+    Assert.assertNotNull(match);
+  }
+
+}
