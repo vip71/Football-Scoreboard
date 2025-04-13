e814278f4bacb836ffe0aad596ab3a37470057de
vip71 <107436192+vip71@users.noreply.github.com>
2025-04-13 16:03:26 +0200

    add handling getting invalid match

--- src/main/java/Scoreboard.java
+++ src/main/java/Scoreboard.java
@@ -1,4 +1,8 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.LinkedList;
+import java.util.Optional;
 
 public class Scoreboard {
 
@@ -6,15 +10,29 @@ public class Scoreboard {
 
   private final ScoreboardSummarizer summarizer = new ScoreboardSummarizer(startedMatches);
 
+  private static final Logger logger = LoggerFactory.getLogger(Scoreboard.class);
+
   public void startMatch(Match match) {
     startedMatches.addFirst(match);
   }
 
   public Match getMatch(String homeTeam, String awayTeam) {
+    return
+        filterFirstMatch(homeTeam, awayTeam)
+        .orElseGet(() -> {
+          handleMissingMatch(homeTeam, awayTeam);
+          return null;
+        });
+  }
+
+  private Optional<Match> filterFirstMatch(String homeTeam, String awayTeam) {
     return startedMatches.stream()
         .filter(match -> match.hasTeams(homeTeam, awayTeam))
-        .findFirst()
-        .orElse(null);
+        .findFirst();
+  }
+
+  private static void handleMissingMatch(String homeTeam, String awayTeam) {
+    logger.info("Match with teams: " + homeTeam + ", " + awayTeam + " has not been found");
   }
 
   public void finishMatch(String homeTeam, String awayTeam) {
